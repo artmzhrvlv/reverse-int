6e830efaddd455a0e710ff2d3b3c365c40af876b
artmzhrvlv <SantaD3zert@gmail.com>
2021-03-16 00:39:20 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
+    var result, num = 0;
+  while (n) {
+    num = n % 10
+    result = (result * 10) + num;
+    n = Math.floor(n / 10);
+  }
+
+  return result;
   
 }
